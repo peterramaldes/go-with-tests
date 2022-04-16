12693bbad229b381520c1221dae98f6797ee799e
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2022-04-16 10:38:11 -0300

    Added language prefix

--- 001-hello-world/hello.go
+++ 001-hello-world/hello.go
@@ -2,16 +2,32 @@ package main
 
 import "fmt"
 
+const french = "French"
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello"
+const spanishHelloPrefix = "Hola"
+const frenchHelloPrefix = "Bonjour"
 
-func Hello(name string) string {
+func Hello(name, lang string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	return fmt.Sprintf("%s, %s", englishHelloPrefix, name)
+	return fmt.Sprintf("%s, %s", greetingPrefix(lang), name)
+}
+
+func greetingPrefix(lang string) (prefix string) {
+	switch lang {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Peter"))
+	fmt.Println(Hello("Peter", ""))
 }
