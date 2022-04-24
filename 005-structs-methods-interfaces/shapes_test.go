ea53726222d6394f2724ae7f4581866c4d540274
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2022-04-24 18:49:30 -0300

    Structs, methods & interfaces

---
+++ 005-structs-methods-interfaces/shapes_test.go
@@ -0,0 +1,37 @@
+package structs_methods_interfaces
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	t.Run("rectangles", func(t *testing.T) {
+		r := Rectangle{10.0, 10.0}
+		got := Perimeter(r)
+		want := 40.0
+
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12.0, 6.0}, want: 72.0},
+		{name: "Circle", shape: Circle{10.0}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v - got %g want %g", tt.shape, got, tt.want)
+			}
+		})
+	}
+
+}
